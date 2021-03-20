467df7e92d646ec38a59dadf56d47e6a3f97bd26
Sonia <serrano_sonia@hotmail.com>
2021-03-19 22:18:11 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+    const element = document.querySelector('#main');
+    element.remove();
+
+    const newHeader = document.createElement('h1');
+    document.body.appendChild(newHeader);
+    newHeader.id = 'victory';
+    newHeader.innerHTML = 'SONIA is the champion';
+
+  
+
+
